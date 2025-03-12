0c33e39e4f6e6c97e69f873b96171194f51c37b9
vijayram8480 <h.vijayram008@gmail.com>
2025-03-12 15:38:22 +0530

    hari

--- sub3.java
+++ sub3.java
@@ -1,6 +1,6 @@
 public class sub3{
     public static void main(String[]args){
-        /*System.out.print("hello");
-        System.out.print("world");*/
+        String name="vijay ram";
+        System.out.println(name);
     }
 }
