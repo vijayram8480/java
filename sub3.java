843efb23f848b675fe82edaeb6a0f13fbae31455
vijayram8480 <h.vijayram008@gmail.com>
2025-03-12 15:08:32 +0530

    world

--- sub3.java
+++ sub3.java
@@ -1,5 +1,6 @@
 public class sub3{
     public static void main(String[]args){
-       System.out.println("hello word");
+        /*System.out.print("hello");
+        System.out.print("world");*/
     }
 }
