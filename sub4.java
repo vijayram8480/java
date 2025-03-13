83a6403ec91839de31fed88ebaf09ebb67a84cd3
vijayram8480 <h.vijayram008@gmail.com>
2025-03-13 10:05:27 +0530

    name

---
+++ sub4.java
@@ -0,0 +1,12 @@
+public class sub4{
+     public static void main(String[]args){
+         String Name="vj";
+         System.out.println("enter the number:"+Name);
+         String Sub="cse";
+         System.out.println("enter your Subject:"+Sub);
+         int Mark=06;
+         System.out.println("enter your mark:"+Mark);
+         String Grade="u";
+         System.out.println("enter your grade:"+Grade);
+     }
+}
