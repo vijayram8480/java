8780d8a1ae1666da575dabb2caf03c2b18e419c4
vijayram8480 <h.vijayram008@gmail.com>
2025-03-13 11:56:00 +0530

    hero

---
+++ com.java
@@ -0,0 +1,13 @@
+public class com{
+    public static void main(String[]args){
+        int str1=3;
+        int str2=3;
+         if(str1==str2){
+            System.out.println("it is equal");
+         }
+         else
+         {
+            System.out.println("it is not equal");
+         }
+  }
+}
