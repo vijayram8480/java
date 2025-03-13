8f6e5863bbdcc66f9dc530e312b16bb91aca97c3
vijayram8480 <h.vijayram008@gmail.com>
2025-03-13 15:26:07 +0530

    system

---
+++ ss.java
@@ -0,0 +1,13 @@
+public class ss{
+    public static void main(String[]args){
+        int age=18;
+        if(age==11){
+            System.out.println("he is mager");}
+            else if(age>=22){
+                System.out.println("he is not mager");
+            }
+            else{
+                System.out.println("he is not mager");
+            }
+    }
+}
