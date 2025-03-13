52bcc7d26331ec519d1cfef75c6562372d159ec4
vijayram8480 <h.vijayram008@gmail.com>
2025-03-13 12:15:45 +0530

    or

---
+++ op2.java
@@ -0,0 +1,11 @@
+public class op2{
+    public static void main(String [] args)
+    {
+        int a=10,b=7;
+        if(a>b || a==b)
+        System.out.println("it is true");
+        else
+        System.out.println("false");
+        
+         }
+}
