80c42fb6f8bb52560e1e405236493659587a2044
vijayram8480 <h.vijayram008@gmail.com>
2025-03-13 11:13:57 +0530

    hero

---
+++ assi.java
@@ -0,0 +1,8 @@
+public class assi{
+    public static void main(String[]args){
+        int x=24;
+        int y=4;
+        System.out.println("Addition:"+(x+y));
+        System.out.println("multi:"+x*y);
+    }
+}
