9879fa9f43f9fd031de5e53476edc1e8743f9a8a
vijayram8480 <h.vijayram008@gmail.com>
2025-03-13 14:46:29 +0530

    mager

---
+++ stt.java
@@ -0,0 +1,10 @@
+public class stt{
+    public static void main(String[]args){
+        int age=18;
+        if(age>17){
+            System.out.println("he is mager");}
+            else{
+                System.out.println("he is not mager");
+            }
+    }
+}
