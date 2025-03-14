2be8980da570ed41f476a2790577d04ef0820018
vijayram8480 <h.vijayram008@gmail.com>
2025-03-14 10:58:37 +0530

    hi

---
+++ g.java
@@ -0,0 +1,11 @@
+public class g{
+    public static void main(String[]args){
+        int a=0;
+do
+{
+            System.out.println(10);
+            a++;
+        }
+        while(a<5);
+    }
+}
