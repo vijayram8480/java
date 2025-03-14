20bc62402edc279055bee8e61a236b2dc1a8631d
vijayram8480 <h.vijayram008@gmail.com>
2025-03-14 13:23:11 +0530

    top

---
+++ h.java
@@ -0,0 +1,10 @@
+public class h{
+    static void mymethod()
+    {
+        System.out.println("multi:"+6*7);
+    }
+    public static void main(String[]args)
+    {
+        mymethod();
+    }
+}
