0039746d5dd6f3a37ecefc3bf26f0789b33b4c11
vijayram8480 <h.vijayram008@gmail.com>
2025-03-14 11:02:13 +0530

    oii

---
+++ gg.java
@@ -0,0 +1,12 @@
+public class gg{
+    public static void main(String[]args){
+        String h="done";
+        int a=0;
+do
+{
+            System.out.println(h);
+            a++;
+        }
+        while(a<5);
+    }
+}
