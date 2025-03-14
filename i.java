838562c42babd7a12418e48244749ae7a78891b1
vijayram8480 <h.vijayram008@gmail.com>
2025-03-14 10:17:32 +0530

    string

---
+++ i.java
@@ -0,0 +1,11 @@
+public class i{
+    public static void main(String[]args){
+        String hy="hello";
+        if(hy.contains("hello")){
+            System.out.println("it is string");
+        }
+        else{
+            System.out.println("it is not a string");
+        }
+    }
+}
