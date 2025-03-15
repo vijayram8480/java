52e2e4f74ec16a053b8d5bbb8babdad28c2576ae
vijayram8480 <h.vijayram008@gmail.com>
2025-03-15 11:12:39 +0530

    coffee

---
+++ coffee.java
@@ -0,0 +1,19 @@
+public class coffee{
+    public static void main(String[]args)
+    {
+        int cof=4;
+        switch(cof){
+            case 1:System.out.println("black coffee");
+            break;
+            case 2:System.out.println("white coffee");
+            break;
+            case 3:System.out.println("tea coffee");
+            break;
+            case 4:System.out.println("love coffee");
+            break;
+            default :
+            System.out.println("the coffee is not available");
+
+        }
+    }
+}
